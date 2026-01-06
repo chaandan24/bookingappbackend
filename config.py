ee020e5fa32b3b67ad4b52f32c828bbf0f2dd330
Chaandan Gadi <chaandan@Chaandans-MacBook-Pro.local>
2026-01-07 04:33:47 +0500

    eafij

--- config.py
+++ config.py
@@ -44,6 +44,8 @@ class Config:
     STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
     STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
     STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET')
+
+    BASIS_THEORY_PRIVATE_KEY = os.getenv('BASIS_THEORY_PRIVATE_KEY')
     
     # Redis Configuration
     REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
