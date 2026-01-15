1012cd58564ae88fa302344ccf88de97d259bb26
Chaandan Gadi <chaandan@Chaandans-MacBook-Pro.local>
2026-01-15 23:16:50 +0500

    efsij

--- app/api/notifications/routes.py
+++ app/api/notifications/routes.py
@@ -8,12 +8,10 @@ import os
 
 notifications_bp = Blueprint('notifications', __name__)
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-key_path = os.path.join(current_dir, '...', 'qimbl-449f5-e1474119430e.json')
 
 # 3. Initialize with the absolute path
 if not firebase_admin._apps:
-    cred = credentials.Certificate(key_path)
+    cred = credentials.Certificate('/home/dossdowncom/dossdownbackend/bookingappbackend/qimbl-449f5-e1474119430e.json')
     firebase_admin.initialize_app(cred)
 @notifications_bp.route('/register-token', methods=['POST'])
 @jwt_required()
